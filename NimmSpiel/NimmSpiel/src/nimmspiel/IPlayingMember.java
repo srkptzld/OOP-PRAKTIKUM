ab25f27ae4e3f207f4362a14365adfa53f7099a5
Sirk Petzold <sirk.petzold@icloud.de>
2018-04-13 14:41:25 +0200

    OOP-Prak-A1
    + IPlayingMember-inteface (better solution than generalization)

---
+++ NimmSpiel/NimmSpiel/src/nimmspiel/IPlayingMember.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nimmspiel;
+
+/**
+ *
+ * @author sirkpetzold
+ */
+
+// interface für die Zug-Methode
+public interface IPlayingMember {
+    public int Zug(int verbleibendeSteine);
+}
