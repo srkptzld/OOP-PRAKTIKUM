62515914e1d85b1a3c13813aa55f699ef18b380b
Sirk Petzold <sirk.petzold@icloud.de>
2018-04-13 14:43:14 +0200

    OOP-Prak-A1
    + run-method logic

--- NimmSpiel/NimmSpiel/src/nimmspiel/Terminal.java
+++ NimmSpiel/NimmSpiel/src/nimmspiel/Terminal.java
@@ -13,13 +13,27 @@ public class Terminal {
     
     private Spiel _spiel;
     
+    
+    // Konstruktor, Instanziert das Feld _spiel und gibt die Spielregeln aus
     public Terminal(){
-        
+        _spiel = new Spiel();
+        Ausgabe.SpielRegeln();
     }
     
+    // Schleife bis keine Steine mehr ziehbar
     public void Run()
     {
-        
+        Spieler aktSpieler;
+        while (_spiel.getSteine() > 0){
+            if (_spiel.getDran > 0)
+                aktSpieler = _spiel.getCom()
+            else
+                aktSpieler = _spiel.getSpieler();
+            
+            _spiel.verringereSteineUm(aktSpieler.Zug());
+            Ausgabe.ZugEnde(_spiel);
+        }
+        Ausgabe.SpielEnde(spiel);
     }
         
 }
