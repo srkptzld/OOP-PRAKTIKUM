7040cbfeebec6eaf94cd41e7d65f10094bd2a01a
Sirk Petzold <sirk.petzold@icloud.de>
2018-04-13 13:04:58 +0200

    OOP-PRAK-A1
    + project NimmSpiel + created classes Com, Main, Spieler, Spiel, Terminal, Ausgabe

---
+++ NimmSpiel/NimmSpiel/src/nimmspiel/Com.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nimmspiel;
+
+/**
+ *
+ * @author sirkpetzold
+ */
+public class Com extends Spieler{
+   
+    private String _name;
+    
+    public Com(String name){
+        
+    }
+    
+    public int Zug(int verbleibendeSteine){  //begründen warum anders als in UML
+        
+    }
+}
---
+++ NimmSpiel/NimmSpiel/src/nimmspiel/Terminal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nimmspiel;
+
+/**
+ *
+ * @author sirkpetzold
+ */
+public class Terminal {
+    
+    private Spiel _spiel;
+    
+    public Terminal(){
+        
+    }
+    
+    public void Run()
+    {
+        
+    }
+        
+}
