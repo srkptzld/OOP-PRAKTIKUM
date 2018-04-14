16c3f84118ba2d07c990905fe49bea15dfa943bf
Funbold <38350727+Funbold@users.noreply.github.com>
2018-04-14 14:45:41 +0200

    OOP-Prak-A1
    #create slightly changed
    + basic methods, may change

--- NimmSpiel/NimmSpiel/src/nimmspiel/Spiel.java
+++ NimmSpiel/NimmSpiel/src/nimmspiel/Spiel.java
@@ -16,13 +16,65 @@ public class Spiel {
 		// keine Methode SetSteine sondern VerringereSteineUm(int anzahl) !! macht mehr sinn
                 // Methode getCom(): Com auch ins UML..
 		
-		_anzSteine = (int)(Math.random() * 10) + 20; // Legt einen Anfangswert zwischen 20 & 30 für den Steinhaufen fest
+		this._anzSteine = (int)(Math.random() * 10) + 20; // Legt einen Anfangswert zwischen 20 & 30 für den Steinhaufen fest
 		
-		_spieler = new Spieler(name); // Erzeugt Spieler
-		_com = new Spieler(Com); // Erzeugt Com
+		this._spieler_spieler = new Spieler(name); // Erzeugt Spieler
+		this._spieler_com = new Spieler(COM); // Erzeugt Com
+		
+		this._rundenZaehler = 1; // Setzt Runde auf 1
 		
 	}
     
+	public void Run() {
+		
+	}
 	
+	public Spieler getSpieler() {
+		
+		return _spieler;
+		
+	}
+	
+	public Com getCom() {
+		
+		return _com;
+		
+	}
+	
+	public int getSteine() {
+		
+		return _anzSteine;
+		
+	}
+	
+	public byte getDran() {
+		
+		return dran;
+		
+	}
+	
+	public int getRunde() {
+		
+		return rundenZaehler;
+	
+	}
+	
+	public void verringereSteineUm(int anzahl) { // Verrignert nach Zug den Steinhaufen um angegebenen Wert
+		
+		_anzSteine = _anzSteine - anzahl;
+		
+	}
+	
+	public void setDran(byte dran) { 
+		
+		_dran = dran;
+		
+	}
+	
+	public void erhoeheRunde() { // Nächste Runde
+		
+		rundenZaehler = rundenZaehler + 1;
+		
+	}
 	
 }
