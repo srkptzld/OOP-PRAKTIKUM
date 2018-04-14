eb56b26949ce605e1e01742b8336cc2ee3cd115f
Funbold <38350727+Funbold@users.noreply.github.com>
2018-04-14 17:38:47 +0200

    OOP-Prak-A1
    + simple greeting method added

--- NimmSpiel/NimmSpiel/src/nimmspiel/Ausgabe.java
+++ NimmSpiel/NimmSpiel/src/nimmspiel/Ausgabe.java
@@ -7,4 +7,11 @@ package nimmspiel;
 /// Gibt Informationstext aus
 public class Ausgabe {
     //alle Methoden static
+	
+	public static void Begruessung(){
+		
+		System.out.println("Herzlich willkommen. Zum Spielen Namen eingeben und ENTER drücken:");
+		
+	}
+	
 }
