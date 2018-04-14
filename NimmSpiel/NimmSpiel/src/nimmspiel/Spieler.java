6a298372caf660747b5fb6f94081e44e9c3bd4ff
Funbold <38350727+Funbold@users.noreply.github.com>
2018-04-14 18:12:52 +0200

    OOP-Prak-A1
    #changed Zug-method, so it does not accept unacceptable enters
    + added getName

--- NimmSpiel/NimmSpiel/src/nimmspiel/Spieler.java
+++ NimmSpiel/NimmSpiel/src/nimmspiel/Spieler.java
@@ -7,23 +7,44 @@ import java.util.Scanner;
  * @author funbold
  */
 public class Spieler implements IPlayingMember {
+	
+	private String _name;
+	
+	public Spieler(String name) { // Konstruktor
+		
+		_name = name;
+		
+	}
 
     @Override
-    public int Zug(int verbleibendeSteine) {
+    public int Zug(int verbleibendeSteine) { // Nimmt den Zug entgegen und prüft ob regelkonform
         
+    	boolean konform = false;
     	int abzug;
     	
-    	System.out.println("Wieviele Steine möchtest du nehmen?");
-    	Scanner scanner = new Scanner(System.in);
-        abzug = scanner.nextLine();
-        
-        return abzug;
+    	System.out.println("Wieviele Steine möchten Sie nehmen?");
+    	
+    	while(konform == false) {
+    		
+    		Scanner scanner = new Scanner(System.in);
+            abzug = scanner.nextLine();
+    		
+            if(abzug < 1 || abzug > 3) {
+            	System.out.println("Die Eingabe ist nicht regelkonform.");
+            	System.out.println("Bitte wählen Sie eine Zahl von 1 bis 3.");
+            }else {
+            	konform == true;
+            }
+    		
+    	}
+    	
+    	return abzug;
     	
     }
 
     @Override
     public String getName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return _name;
     }
 
 }
