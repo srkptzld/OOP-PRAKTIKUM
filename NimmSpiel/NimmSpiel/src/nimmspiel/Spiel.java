3efdb2ee751f8e659024d92774c2cfe45d9f9178
Funbold <38350727+Funbold@users.noreply.github.com>
2018-04-14 14:30:31 +0200

    OOP-Prak-A1
    + create (anzSteine, spieler, com)

--- NimmSpiel/NimmSpiel/src/nimmspiel/Spiel.java
+++ NimmSpiel/NimmSpiel/src/nimmspiel/Spiel.java
@@ -6,9 +6,23 @@ package nimmspiel;
  */
 public class Spiel {
 	
+	private Spieler _spieler;
+	private Com _com;
+	private int _anzSteine;
+	private byte _dran;
+	private int _rundenZaehler;
+	
 	public Spiel(String name) {
 		// keine Methode SetSteine sondern VerringereSteineUm(int anzahl) !! macht mehr sinn
                 // Methode getCom(): Com auch ins UML..
+		
+		_anzSteine = (int)(Math.random() * 10) + 20; // Legt einen Anfangswert zwischen 20 & 30 für den Steinhaufen fest
+		
+		_spieler = new Spieler(name); // Erzeugt Spieler
+		_com = new Spieler(Com); // Erzeugt Com
+		
 	}
     
+	
+	
 }
