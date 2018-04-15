8da3d7cb3702b2d88fd96e345bc1abd73a64447a
Sirk Petzold <sirk.petzold@icloud.de>
2018-04-15 12:23:51 +0200

    OOP-Prak-A1
    + SpielEnde()-Methode # umlauts corrected # changed output

--- NimmSpiel/NimmSpiel/src/nimmspiel/Ausgabe.java
+++ NimmSpiel/NimmSpiel/src/nimmspiel/Ausgabe.java
@@ -10,26 +10,29 @@ public class Ausgabe {
 	
 	public static void Begruessung(){
 		
-		System.out.println("Herzlich willkommen. Zum Spielen Namen eingeben und ENTER drücken:");
+		System.out.println("Herzlich willkommen. Zum Spielen Namen eingeben und ENTER drÃ¼cken:");
 		
 	}
 	
 	public static void SpielRegeln(int anzSteine) {
 		
 		System.out.println("Die Spielregeln sind simpel:");
-		System.out.println("Auf dem Steinhaufen befinden sich " + anzSteine + " Steine.");
-		System.out.println("Die Spieler nehmen abwechselnd 1-3 Steine vom Haufen, bis keine Steine mehr übrig sind.");
-		System.out.println("Der Spieler, welcher den letzten Stein aufnimmt, hat verloren!");
-		System.out.println("Möge der Bessere gewinnen!");
+		System.out.println("1. Auf dem Steinhaufen befinden sich " + anzSteine + " Steine.");
+		System.out.println("2. Die Spieler nehmen abwechselnd 1-3 Steine vom Haufen, bis keine Steine mehr Ã¼brig sind.");
+		System.out.println("3. Der Spieler, welcher den letzten Stein aufnimmt, hat verloren!");
+		System.out.println("4. MÃ¶ge der Bessere gewinnen!");
 		
 	}
 	
-	public static void ZugEnde(int abgezogeneSteine, int uebrigeSteine) {
-		
-		System.out.println("Es wurden " + abgezogeneSteine + " Steine genommen.");
+	public static void ZugEnde(int abgezogeneSteine, int uebrigeSteine, IPlayingMember spieler) {	
+		if (abgezogeneSteine > 0)
+                    System.out.println("Es wurden " + abgezogeneSteine + " Steine von " + spieler.getName() + " genommen.");
 		System.out.println("Auf dem Haufen befinden sich noch " + uebrigeSteine + " Steine.");
 		
 	}
 	
-	
+        public static void SpielEnde(Spiel spiel ){
+            IPlayingMember spieler = (spiel.getDran() > 0) ? spiel.getCom() : spiel.getSpieler();
+            System.out.println("Nach " + spiel.getRunde() + " Runden hat der Spieler " + spieler.getName() + " verloren!");
+        }
 }
