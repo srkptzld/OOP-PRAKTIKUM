ce8c159416ba5d594ab387a3202408672a236eec
Funbold <38350727+Funbold@users.noreply.github.com>
2018-05-29 11:06:32 +0200

    OOP-Prak-A3
    #Changed 'ByteArrayOutputStream' to 'FileOutputStream' -> works now
    
    + added saveDecrypted -> works

--- XORChiffre/XORChiffre/src/xorchiffre/Ausgabe.java
+++ XORChiffre/XORChiffre/src/xorchiffre/Ausgabe.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.io.FileWriter;
 
 /**
  * 
@@ -22,12 +23,12 @@ public class Ausgabe {
     
     public void saveEncrypted(Encryptor enc){
         
-        ByteArrayOutputStream fop = null;
+        FileOutputStream fop = null;
         ArrayList<byte[]> encrypted = enc.getResult();            
         try { 
             File f = enc.getFile();
               
-            fop = new ByteArrayOutputStream(enc.getResult().size());       
+            fop = new FileOutputStream(f);       
             boolean b;
             if (!f.exists())
                 b = f.createNewFile();
@@ -50,6 +51,39 @@ public class Ausgabe {
                 }
         
         }
+    }
+    
+    public void saveDecrypted(Decryptor dec){
+        
+        FileWriter fop = null;
+        ArrayList<String> decrypted = dec.getResult();            
+        try { 
+            File f = dec.getFile();
+              
+            fop = new FileWriter(f);       
+            boolean b;
+            if (!f.exists())
+                b = f.createNewFile();
+            
+            for (int i = 0; i <= decrypted.size() - 1; i++) {
+               fop.write(decrypted.get(i));
+            }     
+            fop.flush();
+            fop.close();
+
+        } catch (IOException e) {
+                        e.printStackTrace();
+        } finally {
+                try {
+                        if (fop != null) {
+                                fop.close();
+                        }
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+        
+        }
+        
     }
 
 
