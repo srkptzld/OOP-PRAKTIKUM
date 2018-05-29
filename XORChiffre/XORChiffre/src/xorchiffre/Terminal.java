db2b994ddb33392a5ff3c2ab1d07c5229442ca38
Funbold <38350727+Funbold@users.noreply.github.com>
2018-05-29 10:24:45 +0200

    OOP-Prak-A3
    #shifted methods to Terminal

--- XORChiffre/XORChiffre/src/xorchiffre/Terminal.java
+++ XORChiffre/XORChiffre/src/xorchiffre/Terminal.java
@@ -1,11 +1,19 @@
 package xorchiffre;
 
+import java.io.IOException;
 import java.util.*;
 
 /**
  * 
  */
 public class Terminal {
+    
+    /**
+     * 
+     */
+    private Eingabe eingabe;
+    private Ausgabe ausgabe;
+    private IConverter converter;
 
     /**
      * Default constructor
@@ -16,26 +24,22 @@ public class Terminal {
     /**
      * 
      */
-    private Eingabe eingabe;
-
-    /**
-     * 
-     */
-    private Ausgabe ausgabe;
-
-    /**
-     * 
-     */
-    private IConverter converter;
-
-
-
-
-    /**
-     * 
-     */
-    public void Run() {
-        // TODO implement here
+    public void run() throws IOException {
+        
+        Eingabe eingabe = new Eingabe();
+        IConverter test = eingabe.Run();
+        Encryptor enc = (Encryptor)test;
+        enc.convert();
+        System.out.println(enc.getResult());
+        System.out.println();
+        
+        Ausgabe ausgabe = new Ausgabe();
+        ausgabe.saveEncrypted(enc);
+        
+        Decryptor dec = new Decryptor(421, 54773, 259200, 3, enc.getResult());
+        dec.convert();
+        System.out.println(dec._ergebnis);
+                
     }
 
 }
--- XORChiffre/XORChiffre/src/xorchiffre/XORChiffre.java
+++ XORChiffre/XORChiffre/src/xorchiffre/XORChiffre.java
@@ -12,23 +12,10 @@ public class XORChiffre {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {      
         
-        Eingabe eingabe = new Eingabe();
-        IConverter test = eingabe.Run();
-        Encryptor enc = (Encryptor)test;
-        enc.convert();
-        System.out.println(enc.getResult());
-        System.out.println();
-        
-        Ausgabe out = new Ausgabe();
-        out.saveEncrypted(enc);
-        
-        Decryptor dec = new Decryptor(421, 54773, 259200, 3, enc.getResult());
-        dec.convert();
-        System.out.println(dec._ergebnis);
-        
+        Terminal terminal = new Terminal();
+        terminal.run();
        
     }
     
