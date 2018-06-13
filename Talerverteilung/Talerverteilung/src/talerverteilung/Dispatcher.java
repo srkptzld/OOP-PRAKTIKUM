347ba53d47a42336b164feed30601ea8117ac231
Funbold <38350727+Funbold@users.noreply.github.com>
2018-06-13 15:09:41 +0200

    OOP-Prak-A4
    +added Dispatcher for compileability

---
+++ Talerverteilung/Talerverteilung/src/talerverteilung/Dispatcher.java
@@ -0,0 +1,22 @@
+package talerverteilung;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author all
+ */
+public class Dispatcher {
+    
+private int n;
+private int summe;
+private ArrayList<Integer> bulk;
+    
+    public Dispatcher(ArrayList<Integer> _bulk, int _n){
+
+        n = _n;
+        bulk = _bulk;
+
+    }
+    
+}
